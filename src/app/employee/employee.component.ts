9431ff5225cec3581607a59f67092538b3096bb8
chaitanya <chaitanyaja07@gmail.com>
2020-12-11 15:52:25 +0530

    updated

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,28 +1,24 @@
 import { AppRoutingModule } from './../app-routing.module';
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
-
-
   formData = {
-    name: '' ,
+    name: '',
     salary: '',
-   department: ''
-    }
-   userlist = [];
-   employeeDetails(array: FormData): void {
+    department: '',
+  };
+  userlist = [];
+  employeeDetails(array: FormData): void {
     console.log(this.formData);
-    this.userlist.push(this.formData),
-    this.formData = {};
-}
-ondelete(deleteme: any): void  {
-     this.userlist.splice(deleteme, 1);
-}
-   ngOnInit(): void {}
+    this.userlist.push(this.formData), (this.formData = {});
+  }
+  ondelete(deleteme: any): void {
+    this.userlist.splice(deleteme, 1);
+  }
+  ngOnInit(): void {}
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,30 +1,46 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
- export class TodoComponent implements OnInit {
+export class TodoComponent implements OnInit {
 
- todoData = {
-   name : '',
- };
-
- todoList = [];
-
- onEnter(array: todoData): void{
-  console.log(this.todoData);
-  this.todoList.push(this.todoData),
-  this.todoData = {};
- }
- ondelete(deleteme: any): void  {
-  this.todoList.splice(deleteme, 1);
-}
-
-
- ngOnInit(): void {
+  todoData = {
+    name: '',
+  };
+  ngOnInit(): void {
 
   }
+
+  todoList = [];
+
+  onEnter(array: todoData): void {
+    console.log(this.todoData);
+    this.todoList.push(this.todoData), (this.todoData = {});
+  }
+
+  ondelete(deleteme: any): void {
+    this.todoList.splice(deleteme, 1);
+
+  }
+  isedit(editme : any): void {
+    this.todoList.forEach((element: any) => {
+      element['isedit'] = 'false';
+      editme.isedit= true;
+      function element(editme:any) {
+       element();
+      }
+
+    });
+  }
+saveelement(saveme: element): void{
+  saveme.isedit = false;
+}
+close(closeme: element): void{
+  closeme.isedit = false;
+}
 
 }
